c493448b0e42ce6ef044cff9d9bf8f499b748cce
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-03-02 12:56:00 +0100

    net: wireless: nl80211: fix CONFIG_NL80211_TESTMODE usage
    Change-Id: I98fd5d56830e3d8927a1da7d1caae429b7b1ab52
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -6703,13 +6703,13 @@ __cfg80211_alloc_vendor_skb(struct cfg80211_registered_device *rdev,
 	return NULL;
 }
 
+static struct genl_multicast_group nl80211_vendor_mcgrp = {
+	.name = "vendor",
+};
+
 #ifdef CONFIG_NL80211_TESTMODE
 static struct genl_multicast_group nl80211_testmode_mcgrp = {
 	.name = "testmode",
-};
-
-static struct genl_multicast_group nl80211_vendor_mcgrp = {
-	.name = "vendor",
 };
 
 static int nl80211_testmode_do(struct sk_buff *skb, struct genl_info *info)
@@ -6874,9 +6874,11 @@ void __cfg80211_send_event_skb(struct sk_buff *skb, gfp_t gfp)
 	if (data->nla_type == NL80211_ATTR_VENDOR_DATA)
 		genlmsg_multicast_netns(wiphy_net(&rdev->wiphy), skb, 0,
 			nl80211_vendor_mcgrp.id, gfp);
+#ifdef CONFIG_NL80211_TESTMODE
 	else
 		genlmsg_multicast_netns(wiphy_net(&rdev->wiphy), skb, 0,
 			nl80211_testmode_mcgrp.id, gfp);
+#endif
 }
 EXPORT_SYMBOL(__cfg80211_send_event_skb);
 
